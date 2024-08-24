cb6b630a170e63a76a826b4c7cfcd45a37e54d49
Ertuğrul Garipardıç <118535200+ertugrulgaripardic@users.noreply.github.com>
2024-08-24 18:36:57 +0300

    Add files via upload

---
+++ DizilerLab/src/Driver.java
@@ -0,0 +1,23 @@
+
+public class Driver {
+	public static void main(String[] args) {
+		int[] numbers = new int[5];
+		numbers[0] = 5;
+		numbers[1] = 10;
+		numbers[2] = 20;
+		numbers[3] = 30;
+		numbers[4] = 40;
+		
+		
+		System.out.println(numbers.length);
+		Object number;
+		System.out.priln(Arrays.a.toString(number));
+		
+		,
+		for (int i =0; i<5; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+	}
+
+}
